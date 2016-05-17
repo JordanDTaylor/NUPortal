f18f418489699c8eb27831811594f94c063ea92e
Adrian Ordonez <adrianord@gmail.com>
2016-05-16 23:29:58 -0600

    Modified git ignore, created base app.js with module structure and example.

---
+++ app.js
@@ -0,0 +1,32 @@
+var sql = require("seriate");
+var config = {
+    "server": "174.27.132.81", //external = 174.27.132.81, internal = 192.168.0.61
+    "user": "sa",
+    "password": "Password1",
+    "database": "Test"
+};
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(methodOverride());
+sql.setDefaultConfig(config);
+exports.sql = sql;
+//API modules
+var contacts = require('./modules/contacts/contacts-api');
+app.use(contacts);
+
+//Angular route, rest of routes will be handled in angular.
+app.get('*', function(req,res){
+    res.sendfile('./public/index.html');
+});
+
+//====================================//
+app.listen(8080);
+console.log("App listening on port 8080");
---
+++ modules/contacts/contacts-api.js
@@ -0,0 +1,32 @@
+
+var express = require("express");
+var app = module.exports = express();
+var server = require('../../app');
+var sql = server.sql;
+
+app.get('/api/contactInfo', function(req,res){
+    var user_id = req.param('id');
+    sql.execute({
+        query: "SELECT FirstName, lastName FROM Contact.People WHERE Id = @userId",
+        params: {
+            userId: {
+                type: sql.int,
+                val: user_id,
+            }
+        }
+    }).then(function (results){
+        res.json({results});
+    }, function(err){
+        console.log("Something bad happened: ",err);
+    });
+});
+
+app.get('/api/allContactInfo', function(req,res){
+    sql.execute({
+        query: "SELECT Firstname, LastName FROM Contact.People"
+    }).then(function (results){
+        res.json({results});
+    }, function(err){
+        console.log("Something bad happened: ",err);
+    });
+});
