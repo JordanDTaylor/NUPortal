5d3f915255986029bc24f28a45aa82deec726326
tredmon <trevan.redmon@gmail.com>
2016-05-18 15:22:12 -0600

    set catalog component selector

--- Angular/app/catalog/catalog.component.ts
+++ Angular/app/catalog/catalog.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'nu-home',
+    selector: 'nu-catalog',
     template: `
     <div>
         <h1>{{ pageTitle }}</h1>
-        <img [src]="logoUrl" />
     </div>
      `
 })
-export class HomeComponent {
-    pageTitle: string = 'NU Home Demo';
-    logoUrl: string = 'https://media4.giphy.com/media/5D6kjdPsBRYUo/200_s.gif';
+export class CatalogComponent {
+    pageTitle: string = 'NuPortal Course Catalog';
 }
